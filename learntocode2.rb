2f0f5fb4c5e397cd01da54fbdc0af71acc616ec7
matthewhighland77 <ichibangaijin@yahoo.com>
2013-10-23 08:08:15 -0700

    homework continued

---
+++ learntocode2.rb
@@ -0,0 +1,6 @@
+#6.3 Continued, Table of Content
+puts 'Table of Contents' .center(60)
+puts 
+puts 'Chapter 1:  Getting Started' .ljust(40) + ' page 1' .rjust(20)
+puts 'Chapter 2:  Numbers'.ljust(40) +          ' page 9' .rjust(20)
+puts 'Chapter 3:  Letters'.ljust(40) +          ' page 13'.rjust(20)

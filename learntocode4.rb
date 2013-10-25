8c4a17f9fcf86a372baecd68a825b5d45980478b
matthewhighland77 <ichibangaijin@yahoo.com>
2013-10-25 09:07:37 -0700

    Grandma Part 2

--- learntocode4.rb
+++ learntocode4.rb
@@ -1,10 +1,15 @@
-#Deaf Grandma Part 1
+#Deaf Grandma Part 2
 
 puts 'HI GRANDSONNY, WHAT WOULD YOU LIKE TO ASK YOUR GRANDMA?'
+bye_num = 0
 while true
 	tell_her = gets.chomp
-	if tell_her == 'bye' 
-		puts 'SEE YOU SOON!'
+	if tell_her == 'BYE'
+		bye_num = bye_num + 1
+	else bye_num = 0
+	end
+	if bye_num >= 3
+		puts 'SEE YOU LATER GRANDSONNY!!!!'
 	break
 	end
 	if tell_her != tell_her.upcase
